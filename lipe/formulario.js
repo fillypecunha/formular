4027af17c0f37a5523ec5912079260e18606cc81
Fillype da Silva Macedo Cunha <116668901+fillypecunha@users.noreply.github.com>
2022-11-21 21:53:56 -0300

    Add files via upload

---
+++ lipe/formulario.js
@@ -0,0 +1,23 @@
+function vereficar() {
+   let eua = document.getElementById("estad")
+   let opcaoTexto= eua.options[eua.selectedIndex].text;
+let img =document.createElement(`img`)
+img.setAttribute(`id`,`foto` )
+   
+   if (opcaoTexto=="Goiás") {
+    img.setAttribute(`src`,`goias.png`)
+    document.body.style.background =(`src`,`mapago.jfif`)
+    
+   }
+   if (opcaoTexto=="Ceará") {
+      img.setAttribute(`src`,`ceara.png`)
+      document.body.style.background =(`src`,`mapace.jpg`)
+     }
+     if (opcaoTexto=="Distrito Federal") {
+      img.setAttribute(`src`,`federal.png`)
+      document.body.style.background =(`src`,`mapadf.jpg`)
+
+     }
+
+    
+}
